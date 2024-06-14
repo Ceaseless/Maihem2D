923869d5d4f1200575d7e47a6017cca83c7de000
jakob.pippig@googlemail.com <jakob.pippig@googlemail.com>
2024-06-14 18:12:22 +0200

    Changeset: 43 Branch comment: [Adding some kind of visual feedback to attacks ] Added Hit Effect for Player Attack, Instantiate and Destroy for now

--- Assets/Scripts/Attacks/MeleeAttack.cs
+++ Assets/Scripts/Attacks/MeleeAttack.cs
@@ -7,9 +7,11 @@ namespace Maihem.Attacks
     [CreateAssetMenu(menuName = "Attack Strategies/Melee Attack", order = 0)]
     public class MeleeAttack : AttackStrategy
     {
+        [SerializeField] private GameObject hitEffect;
         public override void Attack(Vector2Int position, Vector2Int direction)
         {
             var target = position + direction;
+            Instantiate(hitEffect, MapManager.Instance.CellToWorld(target),Quaternion.identity);
             if (GameManager.Instance.TryGetActorOnCell(target, out var actor))
             {
                 actor.TakeDamage(Damage);
