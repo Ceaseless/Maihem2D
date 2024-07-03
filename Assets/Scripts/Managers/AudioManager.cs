5b7544c52742a9baa3101bb8ba647a4473014641
luckssmith@googlemail.com <luckssmith@googlemail.com>
2024-07-03 12:06:49 +0200

    Changeset: 220 Added option to play delayed SFX, added minimal time between SFX

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace Maihem.Managers
 {
@@ -6,7 +8,14 @@ namespace Maihem.Managers
     {
         public static AudioManager Instance { get; private set; }
 
-        [SerializeField] private AudioSource soundFXSource;
+        [SerializeField] private float minimalTimeBetweenSfx;
+        
+        [SerializeField] private AudioSource soundFXSourcePrefab;
+        [SerializeField] private int initialPoolSize;
+        [SerializeField] private int poolGrowthStep;
+
+        private List<AudioSource> _sfxSourcePool;
+        private float _lastSfxTimestamp;
         
         private void Awake()
         {
@@ -18,6 +27,24 @@ namespace Maihem.Managers
             {
                 Destroy(gameObject);
             }
+
+            InitializeSfxSourcePool();
+
+        }
+
+        private void InitializeSfxSourcePool()
+        {
+            _sfxSourcePool = new List<AudioSource>(initialPoolSize);
+            for (var i = 0; i < initialPoolSize; i++)
+            {
+                var newSource = Instantiate(soundFXSourcePrefab, transform);
+                _sfxSourcePool.Add(newSource);
+            }
+        }
+
+        public void PlaySoundFXDelayed(AudioClip audioClip, Vector3 sourcePosition, float volume, float delay)
+        {
+            StartCoroutine(DelayedSfx(audioClip, sourcePosition, volume, delay));
         }
 
         public void PlaySoundFX(AudioClip audioClip, Vector2Int cell, float volume)
@@ -28,12 +55,44 @@ namespace Maihem.Managers
 
         public void PlaySoundFX(AudioClip audioClip, Vector3 sourcePosition, float volume)
         {
-            var source = Instantiate(soundFXSource, sourcePosition, Quaternion.identity);
+            if (!IsAllowedToPlay())
+            {
+                PlaySoundFXDelayed(audioClip, sourcePosition, volume, minimalTimeBetweenSfx);
+                return;
+            }
+            var source = GetSfxAudioSource();
+            source.transform.position = sourcePosition;
             source.clip = audioClip;
             source.volume = volume;
             source.Play();
-            var clipLength = audioClip.length;
-            Destroy(source.gameObject, clipLength);
+            _lastSfxTimestamp = Time.time;
+        }
+
+        private AudioSource GetSfxAudioSource()
+        {
+            foreach (var potentialSource in _sfxSourcePool)
+            {
+                if (!potentialSource.isPlaying) return potentialSource;
+            }
+
+            for (var i = 0; i < poolGrowthStep; i++)
+            {
+                var newSource = Instantiate(soundFXSourcePrefab, transform);
+                _sfxSourcePool.Add(newSource);
+            }
+
+            return _sfxSourcePool[^1];
+        }
+
+        private IEnumerator DelayedSfx(AudioClip audioClip, Vector3 sourcePosition, float volume, float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            PlaySoundFX(audioClip, sourcePosition,volume);
+        }
+
+        private bool IsAllowedToPlay()
+        {
+            return Time.time - _lastSfxTimestamp >= minimalTimeBetweenSfx;
         }
     }
 }
